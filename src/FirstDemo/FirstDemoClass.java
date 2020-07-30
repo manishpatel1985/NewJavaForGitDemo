e25f55a272bdb95936658e12f494ac586ad8baef
hetal <hetal@DESKTOP-BPNON4T>
2020-07-30 17:17:54 +0100

    This is my FirstDemoCommitt

---
+++ src/FirstDemo/FirstDemoClass.java
@@ -0,0 +1,16 @@
+package FirstDemo;
+
+public class FirstDemoClass {
+
+	public static void main(String[] args) {
+		String hunger = "Yes";
+		
+		if(hunger.equals("Yes")) {
+			System.out.println("i am hungry");
+		}else {
+			System.out.println("i am not hungry");
+		}
+
+	}
+
+}
